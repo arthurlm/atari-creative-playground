7d80d31eebae28c70af5979ee1957d73dd408a80
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2022-05-21 16:57:19 +0200

    Add atari const

--- src/includes/const.h
+++ src/includes/const.h
@@ -1,8 +1,20 @@
 #ifndef _CONST_H_
 #define _CONST_H_
 
+#include <stdint.h>
+
 // Define some constant to share accross all project.
 
-#define FRACTIONAL_NUMBER_PRECISION_SCALE 10000
+// Define timers
+// see: https://freemint.github.io/tos.hyp/en/xbios_datetime.html#Xbtimer
+#define TIMER_A 0      // usable for own programs
+#define TIMER_HBLANK 1 // Horizontal blank interrupt
+#define TIMER_SYSTEM 2 // 200Hz system timer
+#define TIMER_BAUD 3   // Generator for the Baud rates
+
+#define INTERRUPTION_SERVICE_ADDRESS (uint8_t *)0xFFFFFA0FL
+
+const uint8_t END_OF_INTERRUPT_TIMER_A = ~(1 << 5);
+const uint8_t END_OF_INTERRUPT_TIMER_HBLANK = ~(1 << 0);
 
 #endif // _CONST_H_
